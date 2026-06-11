32214dce726633a15820baff9748552bd5687834
croblexvfx-hub <croblex.vfx@gmail.com>
2026-06-11 14:52:16 +0200

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,20 +1,5 @@
-const BRIDGE_VERSION = "Bridge v81.8";
-const INSPECT = [
-    { nombre: "XR8", objeto: () => window.XR8 },
-    { nombre: "XR8.XrController", objeto: () => window.XR8?.XrController },
-    { nombre: "XR8.XrController.pipelineModule", objeto: () => window.XR8?.XrController?.pipelineModule },
-    { nombre: "XR8.Threejs", objeto: () => window.XR8?.Threejs },
-    { nombre: "XR8.Threejs.pipelineModule", objeto: () => window.XR8?.Threejs?.pipelineModule },
-    { nombre: "XR8.CameraPixelArray", objeto: () => window.XR8?.CameraPixelArray },
-    { nombre: "XR8.CameraPixelArray.pipelineModule", objeto: () => window.XR8?.CameraPixelArray?.pipelineModule },
-    { nombre: "XR8.GlTextureRenderer", objeto: () => window.XR8?.GlTextureRenderer },
-    { nombre: "XR8.Platform", objeto: () => window.XR8?.Platform },
-    { nombre: "XR8.XrConfig", objeto: () => window.XR8?.XrConfig },
-    { nombre: "XR8.XrDevice", objeto: () => window.XR8?.XrDevice },
-    { nombre: "XR8.Vps", objeto: () => window.XR8?.Vps },
-];
+const BRIDGE_VERSION = "Bridge v81.9";
 
-let inspectIndex = 0;
 const panel = document.createElement("div");
 
 panel.style.position = "fixed";
@@ -64,117 +49,62 @@ window.addEventListener("xrloaded", () => {
                 "\n\nonUpdate";
         },
 
-        onProcessCpu() {
-            panel.textContent =
-                BRIDGE_VERSION +
-                "\n\nonProcessCpu";
-        },
+        onProcessCpu(...args) {
 
-        onProcessGpu() {
-            panel.textContent =
-    BRIDGE_VERSION +
-    "\n\nGPU OK\n\n" +
-    "Time: " + Date.now();
+    let texto =
+        BRIDGE_VERSION +
+        "\n\nonProcessCpu\n\n";
+
+    texto += "args = " + args.length;
+
+    if (args.length > 0) {
+
+        texto += "\n\n";
+
+        try {
+
+            texto += Object.keys(args[0]).join("\n");
+
+        } catch {
+
+            texto += "ERROR";
+
         }
 
-    });
+    }
 
-});
-panel.style.cursor = "pointer";
+    panel.textContent = texto;
 
-panel.onclick = () => {
+},
 
-    panel.textContent =
-        BRIDGE_VERSION +
-        "\n\nCLICK";
-
-    try {
-
-        XR8.addCameraPipelineModule({
-
-            name: "bridge-click-test",
-
-            onStart() {
-                panel.textContent = BRIDGE_VERSION + "\n\nonStart";
-            },
-
-            onUpdate() {
-                panel.textContent = BRIDGE_VERSION + "\n\nonUpdate";
-            },
-
-            onProcessCpu() {
-                panel.textContent = BRIDGE_VERSION + "\n\nonProcessCpu";
-            },
-
-            onProcessGpu(...args) {
+        onProcessGpu(...args) {
 
     let texto =
         BRIDGE_VERSION +
         "\n\nonProcessGpu\n\n";
 
-    const f = args[0].frameStartResult;
+    texto += "args = " + args.length;
 
-texto +=
-    "orientation: " + f.orientation + "\n" +
-    "repeatFrame: " + f.repeatFrame + "\n" +
-    "frameTime: " + f.frameTime + "\n" +
-    "videoTime: " + f.videoTime + "\n" +
-    "textureWidth: " + f.textureWidth + "\n" +
-    "textureHeight: " + f.textureHeight;
+    if (args.length > 0) {
 
-    panel.textContent = texto;
+        texto += "\n\n";
 
-}
+        try {
 
-        });
+            texto += Object.keys(args[0]).join("\n");
 
-    } catch (e) {
+        } catch {
 
-        panel.textContent =
-            BRIDGE_VERSION +
-            "\n\nERROR:\n" +
-            e.message;
-
-    }
-
-};
-
-/*
-setInterval(() => {
-
-    let texto = BRIDGE_VERSION + "\n\n";
-
-    const actual = INSPECT[inspectIndex];
-
-    texto += actual.nombre + "\n\n";
-
-    try {
-
-        const obj = actual.objeto();
-
-        if (!obj) {
-
-            texto += "(null)";
-
-        } else {
-
-            texto += "Tipo: " + typeof obj + "\n\n";
-
-if (typeof obj === "function") {
-    texto += obj.toString().substring(0, 500);
-} else {
-    texto += Object.keys(obj).join("\n");
-}
+            texto += "ERROR";
 
         }
 
-    } catch (e) {
-
-        texto += "ERROR";
-
     }
 
     panel.textContent = texto;
 
-}, 250);
-*/
+}
+
+    });
+
+});
